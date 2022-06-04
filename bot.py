9faa34d45b961e50a3828b3d4424f0eb8fd190ac
Ruslan Zhonin <ruslan.zhonin@triplenext.com>
2022-06-04 11:42:43 +0300

    finish parser

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import telebot
+from news_parser import Parser
 
 bot = telebot.TeleBot('5527984135:AAHdNR45WKfP5R4zLE7rtNYDNXeabq6S8ck')
 
 CHANNEL_NAME = '@slayer_parser_bot'
 
-# @bot.message_handler(content_types=['text', 'audio', 'video'])
+news_list = Parser('кошелек')
+print(news_list.get_news())
+# @bot.message_handler(content_types=['text'])
 # def get_message():
 #     pass
 
--- news_parser.py
+++ news_parser.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import re
 
 
 categories = {
@@ -18,10 +19,25 @@ class Parser:
     def get_news(self):
         news = requests.get(self.category_url)
         html = bs(news.content, 'html.parser')
-        for el in html.select('.aubli_data > a'):
-            self.news_list.append(el.text)
-        print(self.news_list)
+        # print(html.select(".news-tidings__item"))
+        for el in html.select(".news-tidings__list >.news-tidings__item_condensed"):
+            title = el.select_one(".news-tidings__clamping > .news-tidings__subtitle span").text
+            image = el.select_one(".news-tidings__image > picture > img").get('src')
+            text = re.sub(r'\s+', ' ', el.select_one(".news-tidings__speech").text)
+            pub_date = re.sub(r'\s+', ' ', el.select_one(".news-tidings__detail .news-tidings__time").text)
+            self.news_object_creator(title, image, text, pub_date)
         return self.news_list
+    
+    def news_object_creator(self, title, image_src, short_text, pub_date):
+        news_object = {
+            'title': title,
+            'image_src': image_src,
+            'short_text': short_text,
+            'pub_date': pub_date
+        }
+        self.news_list.append(news_object)
 
-parser = Parser('кошелек')
-parser.get_news()
+
+# parser = Parser('кошелек')
+# parser.get_news_html()
+
