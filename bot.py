73a4ff6f024967460c14a931cf549d9b6ee35226
Ruslan Zhonin <ruslan.zhonin@triplenext.com>
2022-06-04 20:23:36 +0300

    delete useless code, remove useless response, change response messages

--- bot.py
+++ bot.py
@@ -10,22 +10,24 @@ options_array = ['кошелек', 'лайфстайл', 'авто', 'техно
 
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Привет, выбери категорию новостей')
     keyboard = utils.generate_keyboard('кошелек', 'лайфстайл', 'авто', 'технологии', 'недвижимость')
-    bot.send_message(m.chat.id, m, reply_markup=keyboard)
+    bot.send_message(m.chat.id, 'Привет, выбери категорию новостей', reply_markup=keyboard)
+    print(m)
 
 
 @bot.message_handler(content_types=['text'])
 def get_message(msg):
     try:
+        keyboard = utils.generate_keyboard('кошелек', 'лайфстайл', 'авто', 'технологии', 'недвижимость')
         news_parse = Parser(msg.text)
         news_list = news_parse.get_news()
         for news in news_list:
-            message = f"{news['title']}\n {news['image_src']}\n {news['short_text']}\n {news['pub_date']}"
-            bot.send_message(msg.chat.id, message)
+            message = f"*{news['title']}*\n\n {news['short_text']}\n\n {news['link']}\n\n {news['pub_date']}"
+            bot.send_photo(msg.chat.id, news['image_src'], caption=message, parse_mode="Markdown")
+        bot.send_message(msg.chat.id, 'Выбери категорию новостей, открой иконку списка, возле иконки смайлов', reply_markup=keyboard)
     except:
         if msg.text not in options_array:
-            bot.reply_to(msg, "а вот йух тебе")
+            bot.reply_to(msg, "Выбери из предложенных опций, открой иконку опций возле иконки смайлов, пес!!!")
 
 bot.polling(none_stop=True, interval=0)
 
--- news_parser.py
+++ news_parser.py
@@ -2,18 +2,14 @@ from bs4 import BeautifulSoup as bs
 import requests
 import re
 
-
-
-
-
 class Parser:
     def __init__(self, category):
         self.categories = {
-            'кошелек': 'https://money.onliner.by/',
-            'лайфстайл': 'https://people.onliner.by/',
-            'авто': 'https://auto.onliner.by/',
-            'технологии': 'https://tech.onliner.by/',
-            'недвижимость': 'https://realt.onliner.by/'
+            'кошелек': 'https://money.onliner.by',
+            'лайфстайл': 'https://people.onliner.by',
+            'авто': 'https://auto.onliner.by',
+            'технологии': 'https://tech.onliner.by',
+            'недвижимость': 'https://realt.onliner.by'
         }
         self.category_url = self.categories[category]
         self.news_list = []
@@ -21,25 +17,22 @@ class Parser:
     def get_news(self):
         news = requests.get(self.category_url)
         html = bs(news.content, 'html.parser')
-        # print(html.select(".news-tidings__item"))
         for el in html.select(".news-tidings__list >.news-tidings__item_condensed"):
             title = el.select_one(".news-tidings__clamping > .news-tidings__subtitle span").text
             image = el.select_one(".news-tidings__image > picture > img").get('src')
             text = re.sub(r'\s+', ' ', el.select_one(".news-tidings__speech").text)
             pub_date = re.sub(r'\s+', ' ', el.select_one(".news-tidings__detail .news-tidings__time").text)
-            self.news_object_creator(title, image, text, pub_date)
+            link = el.select_one(".news-tidings__stub").get('href')
+            self.news_object_creator(title, image, text, link, pub_date)
         return self.news_list
     
-    def news_object_creator(self, title, image_src, short_text, pub_date):
+    def news_object_creator(self, title, image_src, short_text, link, pub_date):
         news_object = {
             'title': title,
             'image_src': image_src,
             'short_text': short_text,
+            'link': self.category_url + link,
             'pub_date': pub_date
         }
         self.news_list.append(news_object)
-
-
-# parser = Parser('кошелек')
-# parser.get_news_html()
 
